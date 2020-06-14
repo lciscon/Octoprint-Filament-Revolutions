c034464cf9c2ff9856536166e210dcfbaf6def4c
lciscon <lciscon@gmail.com>
2020-06-13 19:30:13 -0500

    Update __init__.py

--- octoprint_filamentrevolutions/__init__.py
+++ octoprint_filamentrevolutions/__init__.py
@@ -320,8 +320,8 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.process_queueing,
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.process_queueing,
     }
 
 
